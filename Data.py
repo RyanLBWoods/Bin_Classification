9d1f749bc5c63cbfd3cbd00ce2ef82cdb81e6c01
Lin Bin <linbin@8afbcf72.cs.st-andrews.ac.uk>
2018-04-09 21:38:33 +0100

    train and test

--- Data.py
+++ Data.py
@@ -1,23 +1,24 @@
 import pandas as pd
-from sklearn.cross_validation import train_test_split
-from sklearn import svm
+from sklearn import cross_validation, svm
+import EvaluationFunctions
 
 # Load data
 x = pd.read_csv('./binary/X.csv', header=None)
 y = pd.read_csv('./binary/Y.csv', header=None)
-# Convert y to one-dimensional array
+wl = pd.read_csv('./binary/Wavelength.csv', header=None)
+
+# Convert y and wl to one-dimensional array
 y = y[0]
+wl = wl[0]
 # Split train and test set
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
+x_train, x_test, y_train, y_test = cross_validation.train_test_split(x, y)
 
-print(x_train)
-print(y_train)
-# print(x_test)
-# print(y_test)
-
-clf = svm.SVC(kernel='rbf')
+clf = svm.SVC(kernel='linear')
 clf.fit(x_train, y_train)
 y_predict = clf.predict(x_test)
 
-print(y_predict)
-print(y_test)
+mae = EvaluationFunctions.mae(y_test, y_predict)
+rmse = EvaluationFunctions.rmse(y_test, y_predict)
+
+print('Mean Absolute Error: ', mae)
+print('Rooted Mean Square Error: ', rmse)
