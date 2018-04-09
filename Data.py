dfe400b4709a42c502fcc4ad0d7b5688456dea1f
Lin Bin <linbin@8afbcf72.cs.st-andrews.ac.uk>
2018-04-09 16:05:32 +0100

    try SVM classifier

--- Data.py
+++ Data.py
@@ -1,8 +1,23 @@
 import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn import svm
 
 # Load data
 x = pd.read_csv('./binary/X.csv', header=None)
 y = pd.read_csv('./binary/Y.csv', header=None)
+# Convert y to one-dimensional array
+y = y[0]
+# Split train and test set
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
 
-print(x.head())
-print(y.head())
+print(x_train)
+print(y_train)
+# print(x_test)
+# print(y_test)
+
+clf = svm.SVC(kernel='rbf')
+clf.fit(x_train, y_train)
+y_predict = clf.predict(x_test)
+
+print(y_predict)
+print(y_test)
