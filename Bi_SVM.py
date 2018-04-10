2f8eb741a244a324b597db1ef551e40f9dd77708
Lin Bin <linbin@8afbcf5e.cs.st-andrews.ac.uk>
2018-04-10 15:40:06 +0100

    add output and plot

--- Bi_SVM.py
+++ Bi_SVM.py
@@ -1,24 +1,48 @@
 import pandas as pd
+import time
 from sklearn import cross_validation, svm
-import EvaluationFunctions
+import matplotlib.pyplot as plt
 
 # Load data
 x = pd.read_csv('./binary/X.csv', header=None)
 y = pd.read_csv('./binary/Y.csv', header=None)
 wl = pd.read_csv('./binary/Wavelength.csv', header=None)
+x_test = pd.read_csv('./binary/XToClassify.csv', header=None)
 
 # Convert y and wl to one-dimensional array
 y = y[0]
 wl = wl[0]
-# Split train and test set
-x_train, x_test, y_train, y_test = cross_validation.train_test_split(x, y)
 
+start = time.time()
 clf = svm.SVC(kernel='linear')
-clf.fit(x_train, y_train)
+# Cross validation
+t_mae = - cross_validation.cross_val_score(clf, x, y, cv=5, scoring='neg_mean_absolute_error')
+t_mse = - cross_validation.cross_val_score(clf, x, y, cv=5, scoring='neg_mean_squared_error')
+print('Mean absolute error of 5-fold cross validation：', t_mae)
+print('Mean squared error of 5-fold cross validation：',t_mse)
+# Train model
+start = time.time()
+clf.fit(x, y)
+print('Training cost', time.time() - start, 'second')
+start = time.time()
 y_predict = clf.predict(x_test)
+print('Prediction test cost', time.time() - start, 'second')
 
-mae = EvaluationFunctions.mae(y_test, y_predict)
-rmse = EvaluationFunctions.rmse(y_test, y_predict)
+# Output prediction to csv file
+output = pd.DataFrame(y_predict)
+output.to_csv('./binary/PredictedClass_SVMLinear.csv', index=None, header=None)
 
-print('Mean Absolute Error: ', mae)
-print('Rooted Mean Square Error: ', rmse)
+# Plot output
+plt.figure(figsize=(15, 8))
+ax1 = plt.subplot(111)
+ax1.set_title("Predicted Color of Optical Reflectance Intensity of Spectrum")
+ax1.grid(color='lightgrey', linestyle='-', linewidth=0.5)
+plt.xlabel('Wavelength')
+plt.ylabel('Optical Reflectance Intensity')
+for i in range(len(y_predict)):
+    if y_predict[i] == 0:
+        plt.plot(wl, x_test.iloc[i], 'g', linewidth=0.5)
+    else:
+        plt.plot(wl, x_test.iloc[i], 'r', linewidth=0.5)
+plt.tight_layout()
+plt.show()
