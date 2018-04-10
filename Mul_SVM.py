a9c016b23cfe8c1c2d8fdeaee58aebd1f7cd479b
Lin Bin <linbin@8afbcf5e.cs.st-andrews.ac.uk>
2018-04-10 15:49:46 +0100

    SVM for multiclass

---
+++ Mul_SVM.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import time
+from sklearn import cross_validation, svm
+import matplotlib.pyplot as plt
+
+# Load data
+x = pd.read_csv('./multiclass/X.csv', header=None)
+y = pd.read_csv('./multiclass/Y.csv', header=None)
+wl = pd.read_csv('./multiclass/Wavelength.csv', header=None)
+x_test = pd.read_csv('./multiclass/XtoClassify.csv', header=None)
+
+# Convert y and wl to one-dimensional array
+y = y[0]
+wl = wl[0]
+
+start = time.time()
+clf = svm.SVC(kernel='linear')
+# Cross validation
+t_mae = - cross_validation.cross_val_score(clf, x, y, cv=5, scoring='neg_mean_absolute_error')
+t_mse = - cross_validation.cross_val_score(clf, x, y, cv=5, scoring='neg_mean_squared_error')
+print('Mean absolute error of 5-fold cross validation：', t_mae)
+print('Mean squared error of 5-fold cross validation：',t_mse)
+# Train model
+start = time.time()
+clf.fit(x, y)
+print('Training cost', time.time() - start, 'second')
+start = time.time()
+y_predict = clf.predict(x_test)
+print('Prediction test cost', time.time() - start, 'second')
+# exit(0)
+# Output prediction to csv file
+output = pd.DataFrame(y_predict)
+output.to_csv('./multiclass/PredictedClass_SVMLinear.csv', index=None, header=None)
+
+# Plot output
+plt.figure(figsize=(15, 8))
+ax1 = plt.subplot(111)
+ax1.set_title("Predicted Color of Optical Reflectance Intensity of Spectrum")
+ax1.grid(color='lightgrey', linestyle='-', linewidth=0.5)
+plt.xlabel('Wavelength')
+plt.ylabel('Optical Reflectance Intensity')
+for i in range(len(y_predict)):
+    if y_predict[i] == 0:
+        plt.plot(wl, x_test.iloc[i], 'b', linewidth=0.5)
+    elif y_predict[i] == 1:
+        plt.plot(wl, x_test.iloc[i], 'g', linewidth=0.5)
+    elif y_predict[i] == 2:
+        plt.plot(wl, x_test.iloc[i], 'pink', linewidth=0.5)
+    elif y_predict[i] == 3:
+        plt.plot(wl, x_test.iloc[i], 'r', linewidth=0.5)
+    elif y_predict[i] == 4:
+        plt.plot(wl, x_test.iloc[i], 'y', linewidth=0.5)
+plt.tight_layout()
+plt.show()
